d82c68286ffdde8c0f3d5c24a7ac3cfc37c34a60
phsu40 <phsu40@gatech.edu>
2023-05-03 15:31:32 -0700

    Integrate components to App.js as well as index styling

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './Navigation.module.css';
 
-const Navigation = () => {
+const Navigation = (props) => {
   return (
     <nav className={styles.nav}>
       <ul>
