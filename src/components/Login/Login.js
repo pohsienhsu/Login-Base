f480e938381f7fbd9d1ce7a48d59007981a7e443
phsu40 <phsu40@gatech.edu>
2023-05-03 16:35:25 -0700

    Code clean up" "

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,7 +11,6 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-
   useEffect(() => {
     const timeoutIdentifier = setTimeout(() => {
       console.log("Checking form validaty");
