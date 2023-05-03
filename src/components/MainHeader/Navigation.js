2a6d2ae2f100c2418fdeddafef8f15a0465516dd
phsu40 <phsu40@gatech.edu>
2023-05-03 15:15:11 -0700

    Create main header components

---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from './Navigation.module.css';
+
+const Navigation = () => {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        {props.isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout} >Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
